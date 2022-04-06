b61e659436481009d9b56efb9fc35614a6ef54b7
Edward Heller <ed@edheller.com>
2022-04-06 13:10:06 -0500

    fixing royaly info

--- backend/src/config.js
+++ backend/src/config.js
@@ -66,7 +66,7 @@ const PUBLIC_MINT_START_DATE = "2022-04-03T11:00:00+00:00"; // This is required.
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x86feb8fdb659d9a3f6af92272382436b6c0dd244"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
